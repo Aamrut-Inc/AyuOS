d3328110ee71eabc02a23f45b788c7dde7f8f72b
Vadi <ishaanvadi@gmail.com>
2026-05-29 00:47:24 +0530

    temp debug whoop secret

--- services/wearables/backend/app/main.py
+++ services/wearables/backend/app/main.py
@@ -84,7 +84,7 @@ async def datetime_parse_exception_handler(_: Request, exc: DatetimeParseError) 
 
 
 api.include_router(head_router) 
-@app.get("/debug/whoop")
+@api.get("/debug/whoop")
 def debug_whoop():
     import os
     secret = os.environ.get("WHOOP_CLIENT_SECRET", "")
